3d9db981c5c7e5291f35609c88db2ad2c43cdc28
DjNovovic <djordjijenovovic@gmail.com>
2021-08-28 14:33:39 +0200

    Zoom In and Out
    MagnificationGesture

--- ZoomInAndOut/ContentView.swift
+++ ZoomInAndOut/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentAmount: CGFloat = 0
+    @State var lastAmount: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "heart.fill")
+            .font(.largeTitle)
+            .padding(16)
+            .foregroundColor(.orange)
+            .background(Color.secondary.cornerRadius(16))
+            .scaleEffect(1 + currentAmount + lastAmount)
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        currentAmount = value - 1
+                    }
+                    .onEnded { value in
+                        lastAmount += currentAmount
+                        currentAmount = 0
+                    }
+            )
+        
     }
 }
 
