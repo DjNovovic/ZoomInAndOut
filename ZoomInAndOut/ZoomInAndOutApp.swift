f50a2bdf7741ce168c8752d8d2f93fe58aa4e899
DjNovovic <djordjijenovovic@gmail.com>
2021-08-28 14:31:39 +0200

    Initial Commit

---
+++ ZoomInAndOut/ZoomInAndOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomInAndOutApp.swift
+//  ZoomInAndOut
+//
+//  Created by Đorđije Novović on 28.8.21..
+//
+
+import SwiftUI
+
+@main
+struct ZoomInAndOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
